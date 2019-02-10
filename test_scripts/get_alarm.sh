b4eeb7d4bc7d5de6450f06b264c9a0108c9608a4
Nick Wu <nickwu241@gmail.com>
2019-02-10 13:02:33 -0800

    Update test_scripts

--- test_scripts/get_alarm.sh
+++ test_scripts/get_alarm.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 set -x
 # usage: <user_id>
-curl "localhost:5000/${1}/alarm"
+curl "http://wakeupthe.net:5000/${1}/alarm"
--- test_scripts/set_alarm.sh
+++ test_scripts/set_alarm.sh
@@ -4,4 +4,4 @@ set -x
 # usage: <user_id> <seconds_from_now>
 curl -X POST --data ''"$(date -u -v+${2}S +'%Y-%m-%dT%H:%M:%SZ')"'' \
     -H 'Content-type: application/json' \
-    "http://7fbfdf67.ngrok.io/${1}/alarm"
+    "http://wakeupthe.net:5000/${1}/alarm"
