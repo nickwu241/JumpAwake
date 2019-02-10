6fa7ec3b016783ac390b0a66b2814e029eec9780
Nick Wu <nickwu241@gmail.com>
2019-02-10 13:02:06 -0800

    Impelemnt VS mode for Google Function

--- functions/index.js
+++ functions/index.js
@@ -32,10 +32,12 @@ const app = dialogflow({
     debug: true
 });
 
-const user = 'gordon'
-const endpointBaseUrl = 'http://7fbfdf67.ngrok.io'
+const user1 = 'gordon'
+const user2 = 'marshall'
+const endpointBaseUrl = 'http://wakeupthe.net:5000'
 
-const endpoint = `${endpointBaseUrl}/${user}/alarm`
+const endpoint1 = `${endpointBaseUrl}/${user1}/alarm`
+const endpoint2 = `${endpointBaseUrl}/${user2}/alarm`
 
 app.intent('Set Alarm Intent', (conv, data) => {
     if (!data['time']) {
@@ -45,21 +47,33 @@ app.intent('Set Alarm Intent', (conv, data) => {
     const alarm = new Date(data['time'])
     const alarmISO = alarm.toISOString()
 
-    console.log(`POST ${endpoint}: ${alarmISO}`)
-    return axios.post(endpoint, alarmISO, {
+    console.log(`POST ${endpoint1}: ${alarmISO}`)
+    var p1 = axios.post(endpoint1, alarmISO, {
             headers: {
                 'Content-Type': 'text/plain'
             }
         })
         .then(res => {
             console.log(res)
-            conv.close(`Your alarm is set.`)
             return res
         })
+
+    console.log(`POST ${endpoint2}: ${alarmISO}`)
+    var p2 = axios.post(endpoint2, alarmISO, {
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
+        .then(res => {
+            console.log(res)
+            return res
+        })
+
+    return Promise.all([p1, p2])
+        .then(r => conv.close(`Your alarm is set.`))
         .catch(err => {
             console.error(err)
-            conv.close(`Your alarm is set.`)
-        })
+        });
 })
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
